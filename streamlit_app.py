0f0613daab4f609db6ff1f89d75a5c41796c0e92
Avielar10 <86604981+avielar10@users.noreply.github.com>
2026-05-17 23:42:38 +0300

    Add SEARCH_ON column to fruit options and update API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages.
 import streamlit as st
+import pandas as pd
 from snowflake.snowpark.functions import col
 import requests  
 
@@ -24,7 +25,8 @@ st.write("The name on your smoothie will be: ", name_on_order)
 conn = st.connection("snowflake")
 session = conn.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
@@ -41,8 +43,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      
         st.subheader(fruit_chosen + ' Nutrution Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
   
     st.write(ingredients_string)
